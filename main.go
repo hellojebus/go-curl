64fd8799f3be43782048f1d2e3be5cbc3b512c9e
Jesus Lizama <dev@hellojeb.us>
2018-05-23 22:27:58 -0700

    initial main func

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package go_curl
 
+import (
+	"os"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
 func main(){
+	for _, url := range os.Args[1:] {
+		resp, error := http.Get(url)
+		if error != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+
+	}
 }
